a57794a6fd961b1764a3f5fe200eb6458b2689b6
dave-yang <daworld.ny@gmail.com>
2019-01-05 22:48:32 +0500

    Category controller with its routes

--- Config/routes.php
+++ Config/routes.php
@@ -1,5 +1,30 @@
 <?php
 
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
 return array(
-    
+
+    // Forum category
+    '/%s/module/forum/category/add' => array(
+        'controller' => 'Admin:Category@addAction'
+    ),
+
+    '/%s/module/forum/category/edit/(:var)' => array(
+        'controller' => 'Admin:Category@editAction'
+    ),
+
+    '/%s/module/forum/category/delete/(:var)' => array(
+        'controller' => 'Admin:Category@deleteAction'
+    ),
+
+    '/%s/module/forum/category/save' => array(
+        'controller' => 'Admin:Category@saveAction'
+    )
 );
---
+++ Controller/Admin/Category.php
@@ -0,0 +1,101 @@
+<?php
+
+/**
+ * This file is part of the Bono CMS
+ * 
+ * Copyright (c) No Global State Lab
+ * 
+ * For the full copyright and license information, please view
+ * the license file that was distributed with this source code.
+ */
+
+namespace Forum\Controller\Admin;
+
+use Krystal\Stdlib\VirtualEntity;
+use Cms\Controller\Admin\AbstractController;
+
+final class Category extends AbstractController
+{
+    /**
+     * Creates category form
+     * 
+     * @param \Krystal\Stdlib\VirtualEntity $category
+     * @return string
+     */
+    private function createForm(VirtualEntity $category)
+    {
+        $new = !$category->getId();
+
+        // Append breadcrumbs
+        $this->view->getBreadcrumbBag()->addOne('Forum')
+                                       ->addOne($new ? 'Add new category' : 'Edit the category');
+
+        return $this->view->render('category.form', array(
+            'new' => $new,
+            'category' => $category
+        ));
+    }
+
+    /**
+     * Renders add form
+     * 
+     * @return string
+     */
+    public function addAction()
+    {
+        return $this->createForm(new VirtualEntity);
+    }
+
+    /**
+     * Renders edit form
+     * 
+     * @param int $id
+     * @return string
+     */
+    public function editAction($id)
+    {
+        $category = $this->getModuleService('categoryService')->fetchById($id);
+
+        if ($category) {
+            return $this->createForm($category);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Deletes a category by its ID
+     * 
+     * @param int $id Category ID
+     * @return string
+     */
+    public function deleteAction($id)
+    {
+        $this->getModuleService('categoryService')->deleteById($id);
+
+        $this->flashBag->set('success', 'Selected element has been removed successfully');
+        return 1;
+    }
+
+    /**
+     * Saves a category
+     * 
+     * @return string
+     */
+    public function saveAction()
+    {
+        // Grab raw POST data
+        $input = $this->request->getPost('category');
+
+        $service = $this->getModuleService('categoryService');
+        $service->save($input);
+
+        if ($input['id']) {
+            $this->flashBag->set('success', 'The element has been updated successfully');
+            return 1;
+        } else {
+            $this->flashBag->set('success', 'The element has been created successfully');
+            return $service->getLastId();
+        }
+    }
+}
