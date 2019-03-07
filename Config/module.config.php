e630f4cc8193e74edc58c5417148d18509e56887
dave-yang <daworld.ny@gmail.com>
2019-03-07 20:18:21 +0500

    Update module.config.php

--- Config/module.config.php
+++ Config/module.config.php
@@ -1,9 +1,28 @@
 <?php
 
+/**
+ * Module configuration container
+ */
+
 return array(
- 'name'  => 'Forum',
- 'caption'  => 'Forum',
- 'route' => 'Forum:Admin:Browser@indexAction',
- 'order' => 1,
- 'description' => 'Manage forum on your site'
+    'name'  => 'Forum',
+    'description' => 'Manage forum on your site',
+    'menu' => array(
+        'name' => 'Forum',
+        'icon' => 'fas fa-comments',
+        'items' => array(
+            array(
+                'route' => 'Forum:Admin:Browser@indexAction',
+                'name' => 'View forum'
+            ),
+            array(
+                'route' => 'Forum:Admin:Category@addAction',
+                'name' => 'Add category'
+            ),
+            array(
+                'route' => 'Forum:Admin:Topic@addAction',
+                'name' => 'Add new topic'
+            )
+        )
+    )
 );
